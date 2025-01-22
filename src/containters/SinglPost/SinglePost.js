4b35c90df4d8bbfdfbab3f6ba9e8a3014b8bb975
andrijaDomazet <domazet.andrija@gmail.com>
2025-01-22 23:16:58 +0100

    Prepared for Netlify deploy 22.1

--- src/containters/SinglPost/SinglePost.js
+++ src/containters/SinglPost/SinglePost.js
@@ -7,56 +7,36 @@ import data from "../../all__news.json";
 // import TimeFormat from "../../components/TimeFormat/TimeFormat";
 import BodyText from "../../components/BodyText/BodyText";
 import { Link } from "react-router-dom";
-// import { useIdContext } from "../../shared/IdProvider";
+import { useIdContext } from "../../shared/IdProvider";
 import { useGlobalLocation } from "../../shared/LocationContext";
 // import ContenExBox from "../../components/ContentEx/ContenExBox";
 // import SmallBox from "../Manastiri/Tools/SmallBox";
 // import Calendar from "../../components/Calendar/Calendar";
-
-export default function SinglePost() {
+import { urlTitle2 } from "../../shared/utility";
+export default function SinglePost(props) {
   // let params = useParams();
-  // const { id, slug } = useIdContext();
+  const { id, slug, data } = useIdContext();
   const { pathPart } = useGlobalLocation();
-  // console.log("Params", location, pathPart);
-  const [isNews, setIsNews] = useState({ items: [] });
+  console.log("Params", pathPart);
+  console.log("Data", data);
+  // const [isNews, setIsNews] = useState({ items: [] });
+  const [isNews, setIsNews] = useState(() => setArticleState());
 
-  useEffect(() => {
-    let jsonConcat = data;
-    let post = jsonConcat.filter((post) => {
-      // console.log(urlTitle2(post.title), location.pathname);
+  // useEffect(() => {
+  //   // let jsonConcat = data;
+  //   let post = data.find((post) => {
+  //     console.log("TEST", urlTitle2(post.title), pathPart[1]);
+  //     return urlTitle2(post.title) === pathPart[1];
+  //   });
+  //   console.log("Post", post[0]);
+  //   setIsNews(post[0]);
+  // }, []);
+
+  function setArticleState() {
+    return data.find((post) => {
       return urlTitle2(post.title) === pathPart[1];
     });
-    console.log("Post", post[0]);
-    setIsNews(post[0]);
-  }, []);
-
-  // const currentNews = () => {
-  //   return data;
-  // };
-
-  const urlTitle2 = (title) => {
-    const cyrilic = ["č", "ć", "ž", "š", "đ", ",", ":", "-", "?", "!", "."];
-    const replArray = ["c", "c", "z", "s", "dj", "", "", "", "", "", ""];
-    let regex = /--/gi;
-    let url_title = title
-      .toLowerCase()
-      .split("")
-      .map((x) => {
-        return cyrilic.indexOf(x) === -1 ? x : replArray[cyrilic.indexOf(x)];
-      })
-      .join("")
-      .split(" ")
-      .join("-")
-      .replace(regex, "-");
-    return url_title;
-  };
-  const setTitle = (postTitle) => {
-    // return postTitle.title_2 ? postTitle.title_2 : postTitle.title;
-    return postTitle;
-  };
-  //   const setBody = (body) => {
-  //     return <BodyText bodyText={body} />;
-  //   };
+  }
   const mainPost = () => {
     return (
       <main className="mainContent">
@@ -101,10 +81,31 @@ export default function SinglePost() {
       </main>
     );
   };
+  console.log("Pics", isNews);
   return (
     <div className="singlePost">
-      {/* <AdvModule classes={"adClass__XXL"} size={"xxl"} /> */}
-      <div className="content">{mainPost()}</div>
+      <div className="content">
+        <main className="mainContent">
+          <div className="mainContent-img">
+            <div className="mainContent-title">
+              <h1>{isNews.title_2 || isNews.title}</h1>
+            </div>
+            <img src={`${isNews.pics[0]}`} alt="" />
+            <div className="mainContent-img_source">Foto: {isNews.pics && isNews.pics[1]}</div>
+          </div>
+          <div className="mainContent-body">
+            <div className="mainContent-text">
+              <strong className="mainContent-lead">{isNews.lead}</strong>
+              <BodyText bodyText={isNews.body} />
+              {/* {isNews.info && setBody(isNews.info)} */}
+            </div>
+            <div className="time">
+              {/* Datum: <TimeFormat timePost={isNews.time2} />,  */}
+              Izvor: {isNews.source}
+            </div>
+          </div>
+        </main>
+      </div>
       {/* <section className="sp-2">
         <h2 className="sp-2-title">Manastiri Branicevske eparhije</h2>
         <div className="sp-2-box">
@@ -113,9 +114,7 @@ export default function SinglePost() {
           <SmallBox n={data[3]} />
         </div>
       </section> */}
-      {/* <AdvModule classes={"adClass__XXL"} size={"xxl"} /> */}
       {/* <ContenExBox /> */}
-      {/* <AdvModule classes={"adClass__XXL"} size={"xxl"} /> */}
     </div>
   );
 }
---
+++ src/shared/utility.js
@@ -0,0 +1,16 @@
+export function urlTitle2(title) {
+  const cyrilic = ["č", "ć", "ž", "š", "đ", ",", ":", "-", "?", "!", "."];
+  const replArray = ["c", "c", "z", "s", "dj", "", "", "", "", "", ""];
+  let regex = /--/gi;
+  let url_title = title
+    .toLowerCase()
+    .split("")
+    .map((x) => {
+      return cyrilic.indexOf(x) === -1 ? x : replArray[cyrilic.indexOf(x)];
+    })
+    .join("")
+    .split(" ")
+    .join("-")
+    .replace(regex, "-");
+  return url_title;
+}
