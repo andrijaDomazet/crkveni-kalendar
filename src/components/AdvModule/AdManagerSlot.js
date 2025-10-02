bc5812ee5faf2f52324a3d2d6496b9e8f70952a1
Andrija Domazet <andrija.domazet@ugbb.net>
2025-10-02 15:36:45 +0200

    Fix adSlots and sticky logic

--- src/components/AdvModule/AdManagerSlot.js
+++ src/components/AdvModule/AdManagerSlot.js
@@ -1,32 +1,80 @@
+// import { useEffect, useRef } from "react";
+// import { useGlobalLocation } from "../../shared/LocationContext.js";
+
+// const AdManagerSlot = ({ slotNumber }) => {
+//   const { location } = useGlobalLocation();
+//   const previouslocation = useRef(typeof window !== "undefined" ? sessionStorage.getItem("prevPathname") || "" : "");
+
+//   useEffect(() => {
+//     if (typeof window !== "undefined" && window.googletag) {
+//       window.googletag.cmd.push(function () {
+//         window.googletag.display(slotNumber);
+//       });
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     if (previouslocation.current && previouslocation.current !== location.pathname) {
+//       if (typeof window !== "undefined" && window.googletag) {
+//         window.googletag.cmd.push(function () {
+//           window.googletag.pubads().refresh();
+//         });
+//       }
+//     }
+//     previouslocation.current = location.pathname;
+//     if (typeof window !== "undefined") {
+//       sessionStorage.setItem("prevPathname", location.pathname);
+//     }
+//   }, [location.pathname]);
+
+//   return <div id={slotNumber}></div>;
+// };
+
+// export default AdManagerSlot;
+
 import { useEffect, useRef } from "react";
 import { useGlobalLocation } from "../../shared/LocationContext.js";
 
-const AdManagerSlot = ({ slotNumber }) => {
+const AdManagerSlot = ({ slotNumber, onSlotRenderEnded }) => {
   const { location } = useGlobalLocation();
-  const previouslocation = useRef(typeof window !== "undefined" ? sessionStorage.getItem("prevPathname") || "" : "");
+  const previousLocation = useRef(
+    typeof window !== "undefined"
+      ? sessionStorage.getItem("prevPathname") || ""
+      : ""
+  );
 
   useEffect(() => {
     if (typeof window !== "undefined" && window.googletag) {
       window.googletag.cmd.push(function () {
         window.googletag.display(slotNumber);
+        // 🎯 dodaj listener samo jednom
+        window.googletag
+          .pubads()
+          .addEventListener("slotRenderEnded", (event) => {
+            if (event.slot.getSlotElementId() === slotNumber) {
+              if (typeof onSlotRenderEnded === "function") {
+                onSlotRenderEnded(event); // prosledi parent komponenti
+              }
+            }
+          });
       });
     }
-  }, []);
+  }, [slotNumber, onSlotRenderEnded]);
 
   useEffect(() => {
-    if (previouslocation.current && previouslocation.current !== location.pathname) {
-      if (typeof window !== "undefined" && window.googletag) {
-        window.googletag.cmd.push(function () {
-          window.googletag.pubads().refresh();
-        });
-      }
-    }
-    previouslocation.current = location.pathname;
-    if (typeof window !== "undefined") {
-      sessionStorage.setItem("prevPathname", location.pathname);
-    }
-  }, [location.pathname]);
+    if (!previousLocation.current) previousLocation.current = location.pathname;
 
+    if (previousLocation.current !== location.pathname) {
+      const slot = window.googletag
+        .pubads()
+        .getSlots()
+        .find((s) => s.getSlotElementId() === slotNumber);
+      if (slot) window.googletag.pubads().refresh([slot]);
+    }
+
+    previousLocation.current = location.pathname;
+    sessionStorage.setItem("prevPathname", location.pathname);
+  }, [location.pathname, slotNumber]);
   return <div id={slotNumber}></div>;
 };
 
--- src/components/AdvModule/StickyAd.js
+++ src/components/AdvModule/StickyAd.js
@@ -1,56 +1,95 @@
-import React, { useEffect, useState, useRef } from "react";
+// import React, { useEffect, useState, useRef } from "react";
+// import "./StickyAd.scss";
+// import AdManagerSlot from "./AdManagerSlot.js";
+
+// export default function StickyAd(props) {
+//   const [showSticky, setShowSticky] = useState(true);
+//   const [contentLoaded, setContentLoaded] = useState(false);
+//   const adDivRef = useRef(null);
+
+//   useEffect(() => {
+//     // const checkContentLoaded = () => {
+//     //   const divContent = adDivRef.current;
+//     //   setContentLoaded(adDivRef.current && divContent !== "" && divContent.children[0].clientHeight !== 0);
+//     // };
+//     const checkContentLoaded = () => {
+//       const divContent = adDivRef.current;
+//       const firstChild = divContent?.children?.[0];
+//       const isLoaded = !!firstChild && firstChild.clientHeight > 0;
+//       setContentLoaded(isLoaded);
+//     };
+
+//     // Provera da li je sadržaj u div-u učitan kada se komponenta prvi put ucita
+//     checkContentLoaded();
+
+//     // Proveravajte da li je sadržaj u div-u učitan kada se sadržaj promeni
+//     const observer = new MutationObserver(checkContentLoaded);
+//     if (adDivRef.current) {
+//       observer.observe(adDivRef.current, { childList: true, subtree: true });
+//     }
+//     return () => {
+//       observer.disconnect();
+//     };
+//   }, []);
+
+//   //check on refresh page, redirect from one article to another if is sticky hight greater then 0
+//   useEffect(() => {
+//     // console.log("Sticky Changing article", contentLoaded);
+//   }, [props.adUnitPath]);
+
+//   function handleSticky() {
+//     setShowSticky((prev) => !prev);
+//     // setContentLoaded((prev) => !prev);
+//   }
+//   return (
+//     <div className="sticky-banner">
+//       <div className="sticky-banner-box">
+//         {showSticky && (
+//           <div ref={adDivRef} className="sticky-banner-wrapper">
+//             {/* //change the slot number */}
+//             {/* <AdManagerSlot adUnitPath={props.adUnitPath} slotNumber={"div-gpt-ad-1713694757062-0"} /> */}
+//           </div>
+//         )}
+//         {contentLoaded && <img className="closeButton" src={"/img/closeButton.png"} alt="close-button" onClick={handleSticky} />}
+//       </div>
+//     </div>
+//   );
+// }
+
+import { useState } from "react";
 import "./StickyAd.scss";
 import AdManagerSlot from "./AdManagerSlot.js";
 
 export default function StickyAd(props) {
   const [showSticky, setShowSticky] = useState(true);
   const [contentLoaded, setContentLoaded] = useState(false);
-  const adDivRef = useRef(null);
 
-  useEffect(() => {
-    // const checkContentLoaded = () => {
-    //   const divContent = adDivRef.current;
-    //   setContentLoaded(adDivRef.current && divContent !== "" && divContent.children[0].clientHeight !== 0);
-    // };
-    const checkContentLoaded = () => {
-      const divContent = adDivRef.current;
-      const firstChild = divContent?.children?.[0];
-      const isLoaded = !!firstChild && firstChild.clientHeight > 0;
-      setContentLoaded(isLoaded);
-    };
-
-    // Provera da li je sadržaj u div-u učitan kada se komponenta prvi put ucita
-    checkContentLoaded();
-
-    // Proveravajte da li je sadržaj u div-u učitan kada se sadržaj promeni
-    const observer = new MutationObserver(checkContentLoaded);
-    if (adDivRef.current) {
-      observer.observe(adDivRef.current, { childList: true, subtree: true });
-    }
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-
-  //check on refresh page, redirect from one article to another if is sticky hight greater then 0
-  useEffect(() => {
-    // console.log("Sticky Changing article", contentLoaded);
-  }, [props.adUnitPath]);
-
-  function handleSticky() {
-    setShowSticky((prev) => !prev);
-    // setContentLoaded((prev) => !prev);
-  }
   return (
     <div className="sticky-banner">
       <div className="sticky-banner-box">
         {showSticky && (
-          <div ref={adDivRef} className="sticky-banner-wrapper">
-            {/* //change the slot number */}
-            {/* <AdManagerSlot adUnitPath={props.adUnitPath} slotNumber={"div-gpt-ad-1713694757062-0"} /> */}
+          <div className="sticky-banner-wrapper">
+            <AdManagerSlot
+              adUnitPath={props.adUnitPath}
+              slotNumber={"div-gpt-ad-1713694757062-0"}
+              onSlotRenderEnded={(event) => {
+                if (!event.isEmpty) {
+                  setContentLoaded(true);
+                } else {
+                  setContentLoaded(false);
+                }
+              }}
+            />
           </div>
         )}
-        {contentLoaded && <img className="closeButton" src={"/img/closeButton.png"} alt="close-button" onClick={handleSticky} />}
+        {showSticky && contentLoaded && (
+          <img
+            className="closeButton"
+            src={"/img/closeButton.png"}
+            alt="close-button"
+            onClick={() => setShowSticky(false)}
+          />
+        )}
       </div>
     </div>
   );
