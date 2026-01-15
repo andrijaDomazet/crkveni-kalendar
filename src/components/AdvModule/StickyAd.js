c38a8f1f793e1552eaf98aefde0d36f9ba47cb4a
Andrija Domazet <andrija.domazet@ugbb.net>
2026-01-15 11:31:22 +0100

    Add slot for sticky

--- src/components/AdvModule/StickyAd.js
+++ src/components/AdvModule/StickyAd.js
@@ -1,8 +1,46 @@
+// import { useState } from "react";
+// import "./StickyAd.scss";
+// import AdManagerSlot from "./AdManagerSlot.js";
+
+// export default function StickyAd(props) {
+//   const [showSticky, setShowSticky] = useState(true);
+//   const [contentLoaded, setContentLoaded] = useState(false);
+
+//   return (
+//     <div className="sticky-banner">
+//       <div className="sticky-banner-box">
+//         {showSticky && (
+//           <div className="sticky-banner-wrapper">
+//             <AdManagerSlot
+//               adUnitPath={props.adUnitPath}
+//               slotNumber={"div-gpt-ad-1713694757062-0"}
+//               onSlotRenderEnded={(event) => {
+//                 if (!event.isEmpty) {
+//                   setContentLoaded(true);
+//                 } else {
+//                   setContentLoaded(false);
+//                 }
+//               }}
+//             />
+//           </div>
+//         )}
+//         {showSticky && contentLoaded && (
+//           <img
+//             className="closeButton"
+//             src={"/img/closeButton.png"}
+//             alt="close-button"
+//             onClick={() => setShowSticky(false)}
+//           />
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
 import { useState } from "react";
 import "./StickyAd.scss";
 import AdManagerSlot from "./AdManagerSlot.js";
 
-export default function StickyAd(props) {
+export default function StickyAd({ adUnitPath, slotNumber }) {
   const [showSticky, setShowSticky] = useState(true);
   const [contentLoaded, setContentLoaded] = useState(false);
 
@@ -12,8 +50,8 @@ export default function StickyAd(props) {
         {showSticky && (
           <div className="sticky-banner-wrapper">
             <AdManagerSlot
-              adUnitPath={props.adUnitPath}
-              slotNumber={"div-gpt-ad-1713694757062-0"}
+              adUnitPath={adUnitPath}
+              slotNumber={slotNumber}
               onSlotRenderEnded={(event) => {
                 if (!event.isEmpty) {
                   setContentLoaded(true);
