ec3239dddbe2039ab9bc2f6708df6316ac9225a6
Andrija Domazet <andrija.domazet@ugbb.net>
2026-01-19 15:58:11 +0100

    Add push on second useEffect on adSlot

--- src/components/Boxes/SimpleBox/SimpleBox.js
+++ src/components/Boxes/SimpleBox/SimpleBox.js
@@ -1,35 +1,44 @@
 import { Link, useNavigate } from "react-router-dom";
 import "./SimpleBox.scss";
 
-export default function SimpleBox({ mainTitle, mainBody, linkText, buttonText, classes = "", topNavLink = false }) {
+export default function SimpleBox({
+  mainTitle,
+  mainBody,
+  linkText,
+  buttonText,
+  classes = "",
+  topNavLink = false,
+}) {
   let navigate = useNavigate();
   return (
     <section
       className={`simpleBox ${classes}`}
       {...(topNavLink && {
         onClick: () => navigate(topNavLink),
-        style: { cursor: "pointer", boxShadow: "0 10px 40px -5px rgba(139, 90, 43, 0.2), 0 4px 16px -4px rgba(139, 90, 43, 0.15)" },
+        style: {
+          cursor: "pointer",
+        },
       })}
     >
       <h2>{mainTitle}</h2>
       {/* <p>{mainBody}</p> */}
       <div className="simpleBox-body">
-    <p>
-        {Array.isArray(mainBody) ? (
-          <ul>
-            {mainBody.map((item, index) => {
-              return <li key={index}>{item}</li>;
-            })}
-          </ul>
-        ) : (
-          <Link to={linkText}>{mainBody}</Link>
-        )}
-      </p>
-      {/* <p className="secondBody">
+        <div className="simpleBox-body-wrapper">
+          {Array.isArray(mainBody) ? (
+            <ul>
+              {mainBody.map((item, index) => {
+                return <li key={index}>{item}</li>;
+              })}
+            </ul>
+          ) : (
+            <p to={linkText}>{mainBody}</p>
+          )}
+        </div>
+        {/* <p className="secondBody">
         Test
       </p> */}
       </div>
-  
+
       <div>
         {Array.isArray(buttonText) ? (
           buttonText.map((item, index) => {
--- src/containters/KalendarGodina/KalendarGodina.js
+++ src/containters/KalendarGodina/KalendarGodina.js
@@ -1,22 +1,33 @@
 import "./KalendarGodina.scss";
-import Calendar from "../../components/Calendar/Calendar";
 import AdManagerSlot from "../../components/AdvModule/AdManagerSlot";
 import { lazy, Suspense } from "react";
 import { useIdContext } from "../../shared/IdProvider.js";
-// import { useGlobalLocation } from "../../shared/LocationContext.js";
-import { calendarYears, importantHolidaysPerMonth, monthSerb } from "../../components/Calendar/calendar-data/calendar-data.js";
+import {
+  calendarYears,
+  importantHolidaysPerMonth,
+  monthSerb,
+} from "../../components/Calendar/calendar-data/calendar-data.js";
 import SimpleBox from "../../components/Boxes/SimpleBox/SimpleBox.js";
 import TodayBox from "../../components/Boxes/TodayBox/TodayBox.js";
 
 const WidgetLazy = lazy(() => import("../../UI/Widget/Widget.js"));
-const CalendarMonthsLinksLazy = lazy(() => import("../../components/CalendarMonthsLinks/CalendarMonthsLinks.js"));
-const ZadusniceLazy = lazy(() => import("../../components/Boxes/Zadusnice/Zadusnice.js"));
+const ZadusniceLazy = lazy(() =>
+  import("../../components/Boxes/Zadusnice/Zadusnice.js")
+);
 
 export default function KalendarGodina() {
-  const { currentYear, yearIndex, easterDay,isYear,slug } = useIdContext();
+  const { yearIndex, easterDay, isYear, slug } = useIdContext();
+  const pageYear = Number(slug);
+  const years = [2026, 2027, 2028];
+  const otherYears = years.filter((y) => y !== pageYear);
+  const buttonText = otherYears.map((year) => [
+    `/${year}/`,
+    `Kalendar ${year}. →`,
+  ]);
+  const date = new Date(easterDay);
+  const day = date.getDate();
+  const month = monthSerb[date.getMonth()];
 
-  console.log("ID data", slug);
-  // const { pathPart } = useGlobalLocation();
   return (
     <div className="kalendarGodina">
       <div className="banner-wrapper bilbord">
@@ -26,16 +37,21 @@ export default function KalendarGodina() {
         <div className="kalendarGodina-center">
           <div className="kalendarGodina-title">
             <h1>Pravoslavni kalendar {isYear}.</h1>
-            <h2>Kompletan pregled svih meseci, velikih praznika, posnih dana i zadušnica za {isYear}. godinu po crkvenom kalendaru Srpske Pravoslavne Crkve.</h2>
+            <h2>
+              Kompletan pregled svih meseci, velikih praznika, posnih dana i
+              zadušnica za {isYear}. godinu po crkvenom kalendaru Srpske
+              Pravoslavne Crkve.
+            </h2>
           </div>
           <div className="monthsGrid">
             {monthSerb.map((item, index) => {
               return (
                 <SimpleBox
-                  // classes="orange"
+                  classes="hoverable"
                   topNavLink={`/${isYear}/${item}/`}
-                  // linkText={`/2026/${item}/`}
-                  mainTitle={item.substring(0, 1).toUpperCase() + item.substring(1)}
+                  mainTitle={
+                    item.substring(0, 1).toUpperCase() + item.substring(1)
+                  }
                   mainBody={importantHolidaysPerMonth[index]}
                 />
               );
@@ -51,38 +67,44 @@ export default function KalendarGodina() {
             classes="green"
             // linkText="/2026/april/"
             mainTitle="🍞 Veliki postovi 2026."
-            mainBody={["Vaskršnji post — 23.2. - 11.4.", "Petrovski post — 8.6. - 11.7.", "Gospojinski post — 14.8. - 27.8.", "Božićni post — 28.11. - 6.1."]}
+            mainBody={[
+              "Vaskršnji post — 23.2. - 11.4.",
+              "Petrovski post — 8.6. - 11.7.",
+              "Gospojinski post — 14.8. - 27.8.",
+              "Božićni post — 28.11. - 6.1.",
+            ]}
             // buttonText="Hrišćanski postovi u 2026. godini →"
           />
-          {/* <Calendar soc={false} isMonth2={0} /> */}
+          <SimpleBox
+            classes="orange"
+            mainTitle="Ostale godine"
+            buttonText={buttonText}
+          />
           {/* <div className="banner-wrapper fix-size-horizontal">
             <AdManagerSlot slotNumber={"div-gpt-ad-1750409157804-0"} />
           </div> */}
         </div>
         <div className="kalendarGodina-right">
           <Suspense fallback={<div></div>}>
-            <ZadusniceLazy setYear={2026} boxTitle={`🕯 Zadušnice u ${2026}. godini`} data={calendarYears[0].item_list[yearIndex]} />
+            <ZadusniceLazy
+              setYear={pageYear}
+              boxTitle={`🕯 Zadušnice u ${pageYear}. godini`}
+              data={calendarYears[0].item_list[yearIndex]}
+            />
           </Suspense>
-          <SimpleBox classes="green" linkText="/2026/april/" mainTitle="Vaskrs 2026." mainBody="Vaskrs (Uskrs) u 2026. godini pada na 12. april po novom kalendaru." buttonText="Kalendar za april 2026. →" />
           <SimpleBox
-            classes="orange"
-            // linkText="/2026/april/"
-            mainTitle="Ostale godine"
-            // mainBody="Vaskrs (Uskrs) u 2026. godini pada na 12. april po novom kalendaru."
-            buttonText={[
-              [`/${+slug-1}/`, `Kalendar ${+slug-1}. →`],
-              [`/${+slug+1}/`, `Kalendar ${+slug+1}. →`],
-              // ["/2027/", "Kalendar 2027. →"],
-            ]}
+            classes="green"
+            linkText={`/${pageYear}/${month}/`}
+            mainTitle={`Vaskrs ${pageYear}.`}
+            mainBody={`Vaskrs (Uskrs) u ${pageYear}. godini pada na ${day}. ${month} po novom kalendaru.`}
+            buttonText={`Kalendar za ${month} ${pageYear}. →`}
           />
+
           <div className="banner-wrapper xl_sticky">
             <AdManagerSlot slotNumber={"div-gpt-ad-1750411708088-0"} />
           </div>
         </div>
       </div>
-      {/* <Suspense fallback={<div></div>}>
-        <CalendarMonthsLinksLazy />
-      </Suspense> */}
       <div className="banner-wrapper">
         <AdManagerSlot slotNumber={"div-gpt-ad-1750409277034-0"} />
       </div>
